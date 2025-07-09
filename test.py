c452101e8737682cce4eef692d4e7dc88fd9cc0b
dimst12 <dimasteblev12@gmail.com>
2025-07-09 15:28:16 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+from animalssay import (
+    cat_says,
+    dog_says,
+    frog_says,
+    fox_says,
+    cow_says,
+    pig_says,
+    owl_says,
+)
+
+print(cat_says("Meow!"))
+print(dog_says("Woof!"))
+print(frog_says("Ribbit."))
+print(fox_says("What does the fox say?"))
+print(cow_says("Moooo"))
+print(pig_says("Oink oink"))
+print(owl_says("Hoot hoot!"))
